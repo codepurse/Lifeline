2189555b510875816c9c0cac0640e80ea38da827
Alfon Labadan <47669262+codepurse@users.noreply.github.com>
2020-02-17 16:35:06 +0800

    update

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bc0d879cdeaab62e3365";
+/******/ 	var hotCurrentHash = "238ba44476aade3d9446";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
